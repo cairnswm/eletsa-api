053fdd9ec9f33d5fd88e7d07d73e60d4d72bb8cd
William Cairns <cairnswm@gmail.com>
2025-07-23 09:49:41 +0200

    Add transaction support

---
+++ php/transactions/txconfig.php
@@ -0,0 +1,34 @@
+<?php
+
+$txconfig = [
+    "post" => [
+        "transactions" => "getTransactions"
+    ]
+];
+
+function getTransactions($config)
+{
+    global $userid;
+    $sql = "SELECT
+    t.transaction_date,
+    tt.name AS ticket_type_name,
+    e.title AS event_name,
+    ti.quantity,
+    t.amount,
+    t.payout_amount,
+    t.id AS transaction_id,
+    t.user_id,
+    t.order_id,
+    t.order_item_id,
+    t.ticket_id,
+    t.organizer_id
+FROM transactions t
+LEFT JOIN tickets ti ON t.ticket_id = ti.id
+LEFT JOIN ticket_types tt ON ti.ticket_type_id = tt.id
+LEFT JOIN events e ON ti.event_id = e.id
+WHERE t.organizer_id = ?
+ORDER BY t.transaction_date DESC;
+";
+    $result = executeSQL($sql, [$userid]);
+    return $result;
+}
