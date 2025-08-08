a60f75f91016c878fefae553be1d54fc41ed5dbd
William Cairns <cairnswm@gmail.com>
2025-08-08 08:41:00 +0200

    Fixes to fetching data for feed

--- php/activity/activity_functions.php
+++ php/activity/activity_functions.php
@@ -101,52 +101,54 @@ function getUserActivityFeed($config)
   $userId = $config["where"]["user_id"] ?? null;
   $sql = "
     SELECT af.*, at.template AS template_text,
-      CASE WHEN af.activity_type = 'user_followed' THEN af.reference_id_1 ELSE NULL END AS followed_user_id,
-      CASE WHEN af.activity_type IN ('event_created', 'ticket_purchased') THEN e.title
-           WHEN af.activity_type = 'event_reviewed' THEN e2.title
-           ELSE NULL
-      END AS event_title,
-      CASE WHEN af.activity_type IN ('event_created', 'ticket_purchased') THEN e.start_datetime
-           WHEN af.activity_type = 'event_reviewed' THEN e2.start_datetime
-           ELSE NULL
-      END AS event_date,
-      r.rating AS review_rating,
-      LEFT(r.review, 100) AS review_snippet,
-      tt.name AS ticket_type_name,
-      COALESCE(rt.total_reactions, 0) AS total_reactions,
-      rt.reaction_breakdown,
-      COALESCE(cm.total_comments, 0) AS total_comments
-    FROM (
-      SELECT af.*
-      FROM followers f
-      JOIN user_activity_feed af ON af.user_id = f.followed_user_id
-      WHERE f.follower_user_id = ?
-      ORDER BY af.created_at DESC
-      LIMIT ?
-    ) af
-    LEFT JOIN activity_templates at ON af.template_id = at.id AND at.language_code = 'en'
-    LEFT JOIN events e ON af.activity_type IN ('event_created', 'ticket_purchased') AND e.id = af.reference_id_1
-    LEFT JOIN events e2 ON af.activity_type = 'event_reviewed' AND e2.id = af.reference_id_1
-    LEFT JOIN reviews r ON af.activity_type = 'event_reviewed' AND r.id = af.reference_id_2
-    LEFT JOIN ticket_types tt ON af.activity_type = 'ticket_purchased' AND tt.id = af.reference_id_2
-    LEFT JOIN (
-      SELECT ar.activity_id,
-             COUNT(*) AS total_reactions,
-             JSON_OBJECTAGG(ar.reaction_type, cnt) AS reaction_breakdown
-      FROM (
-        SELECT activity_id, reaction_type, COUNT(*) AS cnt
-        FROM activity_reactions
-        GROUP BY activity_id, reaction_type
-      ) ar
-      GROUP BY ar.activity_id
-    ) rt ON rt.activity_id = af.id
-    LEFT JOIN (
-      SELECT activity_id, COUNT(*) AS total_comments
-      FROM activity_comments
-      WHERE is_visible = 1
-      GROUP BY activity_id
-    ) cm ON cm.activity_id = af.id
+CASE WHEN af.activity_type = 'user_followed' THEN af.reference_id_1 ELSE NULL END AS followed_user_id,
+CASE WHEN af.activity_type IN ('event_created', 'ticket_purchased') THEN e.title
+     WHEN af.activity_type = 'event_reviewed' THEN e2.title
+     ELSE NULL
+END AS event_title,
+CASE WHEN af.activity_type IN ('event_created', 'ticket_purchased') THEN e.start_datetime
+     WHEN af.activity_type = 'event_reviewed' THEN e2.start_datetime
+     ELSE NULL
+END AS event_date,
+r.rating AS review_rating,
+LEFT(r.review, 100) AS review_snippet,
+tt.name AS ticket_type_name,
+t.quantity AS ticket_quantity,
+COALESCE(rt.total_reactions, 0) AS total_reactions,
+rt.reaction_breakdown,
+COALESCE(cm.total_comments, 0) AS total_comments
+FROM (
+  SELECT af.*
+  FROM followers f
+  JOIN user_activity_feed af ON af.user_id = f.followed_user_id
+  WHERE f.follower_user_id = ?
+  ORDER BY af.created_at DESC
+  LIMIT 50
+) af
+LEFT JOIN activity_templates at ON af.template_id = at.id AND at.language_code = 'en'
+LEFT JOIN events e ON af.activity_type IN ('event_created', 'ticket_purchased') AND e.id = af.reference_id_1
+LEFT JOIN events e2 ON af.activity_type = 'event_reviewed' AND e2.id = af.reference_id_1
+LEFT JOIN reviews r ON af.activity_type = 'event_reviewed' AND r.id = af.reference_id_2
+LEFT JOIN tickets t ON af.activity_type = 'ticket_purchased' AND t.id = af.reference_id_2
+LEFT JOIN ticket_types tt ON af.activity_type = 'ticket_purchased' AND tt.id = t.ticket_type_id
+LEFT JOIN (
+  SELECT ar.activity_id,
+         COUNT(*) AS total_reactions,
+         JSON_OBJECTAGG(ar.reaction_type, cnt) AS reaction_breakdown
+  FROM (
+    SELECT activity_id, reaction_type, COUNT(*) AS cnt
+    FROM activity_reactions
+    GROUP BY activity_id, reaction_type
+  ) ar
+  GROUP BY ar.activity_id
+) rt ON rt.activity_id = af.id
+LEFT JOIN (
+  SELECT activity_id, COUNT(*) AS total_comments
+  FROM activity_comments
+  WHERE is_visible = 1
+  GROUP BY activity_id
+) cm ON cm.activity_id = af.id;
   ";
-  $params = [$userId, $limit];
+  $params = [$userId];
   return executeSQL($sql, $params, ['reaction_breakdown', 'metadata']);
 }
